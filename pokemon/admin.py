ed33061cd3ea6aa025ca1820fa9d8f69ce1f9216
Ahmed <ahmed.a.almuhanna@hotmail.com>
2022-08-14 11:05:09 +0300

    my task

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -3,4 +3,30 @@ from .models import Pokemon
 # Register your models here.
 admin.site.register(Pokemon)
 # class PokemonAdmin (Pokemon):
+
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "hp", "active")
+    list_filter = ("active",)
+    list_display_links = ("id", "name")
+    fieldsets = (
+        (None,
+         {"fields":
+             ("name",
+              "type",
+              "hp",
+              "active")
+             
+             }),
+        (
+            "Localizations",
+            {
+                "classes":
+                    ("collapse",),
+                "fields":
+                    ("name_fr",
+                     "name_jp",
+                     "name_ar"),
+            },
+        ),
+    )
     
